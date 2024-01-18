19a64ee0b16d9a7624c4b74889e3f452a8ba7996
sulthanrizal <surie0312@gmail.com>
2024-01-18 14:54:19 +0700

    create actions phonebook

--- phonebook-client/src/actions/PhoneBook.js
+++ phonebook-client/src/actions/PhoneBook.js
@@ -1,6 +1,14 @@
+import axios from "axios"
 
 
+export const request = axios.create({
+    baseURL: 'http://localhost:3001/api/',
+    timeout: 1000,
+    headers: { 'X-Custom-Header': 'foobar' }
+});
 
-// const request = axios.create({
-
-// })
+export const readData = (filter) => dispatch => request.get('phonebook', { params: filter }).then(({ data }) => {
+    dispatch({ type: 'LOAD_CONTACT_SUCCESS', contacts: data, sort: filter })
+}).catch(() => {
+    dispatch({ type: 'LOAD_CONTACT_FAILED' })
+});
