2fb2717003a7f64f7ec573377fba5c9fac7da2e0
sulthanrizal <surie0312@gmail.com>
2024-01-28 19:30:32 +0700

    create app.js

--- phonebook-client/src/App.js
+++ phonebook-client/src/App.js
@@ -14,13 +14,11 @@ function App() {
   const [keyword, setKeyword] = useState('')
   const [avatar, setAvatar] = useState(null)
   const [sort, setSort] = useState('asc')
-  const [isLoading, setIsloading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const formData = new FormData()
   const [item, setItem] = useState([])
   const [totalPages, setTotalPages] = useState(1)
   const [page, setPage] = useState(1)
-
-
-
 
   useEffect(() => {
     const readData = async () => {
@@ -32,7 +30,6 @@ function App() {
           }
         })
         const { phonebook, pages } = response.data
-
         if (phonebook) {
           setItem(phonebook)
           setTotalPages(pages)
@@ -42,7 +39,7 @@ function App() {
         throw error
       }
       finally {
-        setIsloading(false)
+        setIsLoading(false)
       }
     }
     readData()
@@ -71,6 +68,92 @@ function App() {
     }).catch(error => window.alert(error, 'your cant delete'))
   }
 
+  const UpdateAvatar = (id, avatar) => {
+    formData.append('avatar', avatar)
+    axios.put(`http://localhost:3001/api/phonebook/${id}/avatar`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    }).then((response) => {
+      setItem((prevData) => {
+        return [
+          ...prevData.filter(data => data.id !== response.data.id),
+          {
+            id: response.data.id,
+            name: response.data.name,
+            phone: response.data.phone,
+            avatar: response.data.avatar,
+          }
+        ]
+      })
+    }).catch((error) => {
+      console.log(error, 'eror')
+    })
+  }
+
+  // const handleScrollPagination = async () => {
+  //   if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight && !isLoading) {
+  //     try {
+  //       if (page < totalPages) {
+  //         setIsLoading(true)
+  //         const newPage = page + 1
+  //         setPage(newPage)
+  //         const newData = await axios.get('http://localhost:3001/api/phonebook', {
+  //           params: {
+  //             page: newPage
+  //           }
+  //         })
+  //         setItem(prevItem => [...prevItem, ...newData.data.phonebook])
+  //       } else {
+  //         setIsLoading(false)
+  //       }
+  //     } catch (error) {
+  //       console.log(error, 'eror')
+  //     } finally {
+  //       setIsLoading(false)
+  //     }
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScrollPagination)
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScrollPagination)
+  //   }
+  // }, [totalPages], page)
+
+  const handleScroll = async () => {
+    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight && !isLoading) {
+      try {
+        if (page < totalPages) {
+          setIsLoading(true)
+          const newPage = page + 1
+          setPage(newPage);
+          const dataBaru = await axios.get(`http://localhost:3001/api/phonebook`, {
+            params: {
+              page: newPage
+            }
+          })
+          setItem(prevItem => [...prevItem, ...dataBaru.data.phonebook])
+        }
+        else {
+          setIsLoading(false)
+        }
+      }
+      catch (err) {
+        console.log(err)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [totalPages, page])
 
 
   return (
@@ -92,7 +175,7 @@ function App() {
             item={item}
             setItem={setItem}
             isLoading={isLoading}
-            setIsloading={setIsloading} />} />
+            setIsLoading={setIsLoading} />} />
           <Route path="/add" element={<FormAdd
             user={user}
             setUser={setUser}
@@ -100,7 +183,7 @@ function App() {
             setItem={setItem}
             sort={sort}
             setSort={setSort} />} />
-          <Route path='/:id/avatar' element={<FormUpdateAvatar avatar={avatar} setAvatar={setAvatar} item={item} user={user} />} />
+          <Route path='/:id/avatar' element={<FormUpdateAvatar UpdateAvatar={UpdateAvatar} avatar={avatar} setAvatar={setAvatar} item={item} user={user} />} />
           <Route path='*' element={<NoMatch />} />
         </Routes>
       </Router>
