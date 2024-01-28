92d7c2822eb8d10e45cd2bdf28b43ca9eb46e59f
sulthanrizal <surie0312@gmail.com>
2024-01-28 19:31:20 +0700

    create testing to React

--- phonebook-client/src/App.test.js
+++ phonebook-client/src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole('link');
   expect(linkElement).toBeInTheDocument();
 });
+
+
