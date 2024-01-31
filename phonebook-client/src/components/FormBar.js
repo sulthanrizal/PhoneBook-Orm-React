fb45a2433ce1326ae5177fde38265879a569ea5b
sulthanrizal <surie0312@gmail.com>
2024-01-31 20:31:42 +0700

    create components FORM-BAR

--- phonebook-client/src/components/FormBar.js
+++ phonebook-client/src/components/FormBar.js
@@ -5,20 +5,22 @@ import { Link } from "react-router-dom"
 
 
 
-const ButtonSortAsc = ({ sort, setSort }) => {
+const ButtonSortAsc = ({ sort, setSort, setPage }) => {
     return (
         <button className='btnSortAsc' onClick={() => {
             setSort('desc')
+            setPage(1)
         }}>
             <FontAwesomeIcon icon={faArrowDownZA} id='center' />
         </button>
     )
 }
 
-const ButtonSortDesc = ({ sort, setSort }) => {
+const ButtonSortDesc = ({ sort, setSort, setPage }) => {
     return (
         <button className='btnSortDesc' onClick={() => {
             setSort('asc')
+            setPage(1)
         }}>
             <FontAwesomeIcon icon={faArrowUpZA} id='center' />
         </button>
@@ -33,15 +35,16 @@ const ButtonAdd = () => {
     )
 }
 
-export default function FormBar({ keyword, setKeyword, sort, setSort }) {
+export default function FormBar({ setPage, keyword, setKeyword, sort, setSort }) {
     const handleSearch = (event) => {
         const { value } = event.target
         setKeyword(value)
+        setPage(1)
     }
     return (
         <div className='container-formbar'>
             <div className="container-form">
-                {sort === 'asc' || sort.sort == 'asc' ? <ButtonSortAsc sort={sort} setSort={setSort} /> : <ButtonSortDesc sort={sort
+                {sort === 'asc' || sort.sort == 'asc' ? <ButtonSortAsc sort={sort} setSort={setSort} setPage={setPage} /> : <ButtonSortDesc setPage={setPage} sort={sort
                 } setSort={setSort} />}
                 <input className="input-form" value={keyword} placeholder='search your name' onInput={handleSearch} />
                 <Link to='/add'> <ButtonAdd /> </Link>
